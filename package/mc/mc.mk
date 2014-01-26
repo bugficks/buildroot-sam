c02d87e413934dfc552f6722f56d2771db25bc70
Denny <card2000@gmx.net>
2014-01-26 01:04:01 +0100

    mc modify

--- package/mc/mc.mk
+++ package/mc/mc.mk
@@ -3,7 +3,7 @@
 # mc                                                          
 #                                                             
 ############################################################# 
-MC_VERSION:=4.8.0
+MC_VERSION:=4.8.1.6
 MC_SOURCE:=mc-$(MC_VERSION).tar.bz2
 MC_SITE:=http://www.midnight-commander.org/downloads
 MC_INSTALL_TARGET=YES
@@ -15,7 +15,13 @@ MC_CONF_OPT += \
 	--with-screen=ncurses \
 	--with-ncurses-libs=$(STAGING_DIR)/usr/lib \
 	--with-ncurses-includes=$(STAGING_DIR)/usr/include \
-	--disable-rpath
+	--disable-rpath \
+	--enable-static \
+	--disable-share \
+	 --disable-doxygen-doc --disable-doxygen-dot --disable-doxygen-html \
+	--with-edit \
+	--sysconfdir=/usr/etc
+	
 define MC_FIX_NCURSES_INCLUDE_DIR        
 	/bin/mkdir -p $(STAGING_DIR)/usr/include/ncurses
 	/bin/cp -dpf $(STAGING_DIR)/usr/include/curses.h $(STAGING_DIR)/usr/include/ncurses/curses.h
